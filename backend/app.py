8dc92fea926c7ecd0310fcf1385d972ba1cbbcc1
kondalu4835 <yedukondalumogili816@gmail.com>
2026-05-04 21:57:02 +0530

    Updated CORS configuration for deployment

--- backend/app.py
+++ backend/app.py
@@ -9,11 +9,17 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 # Proper CORS for JWT
+# CORS(app, resources={r"/api/*": {
+#     "origins": ["http://localhost:8000", "http://127.0.0.1:8000"],
+#     "allow_headers": ["Content-Type", "Authorization"],
+#     "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+# }})
 CORS(app, resources={r"/api/*": {
-    "origins": ["http://localhost:8000", "http://127.0.0.1:8000"],
+    "origins": ["https://qatar-foundation-portal.onrender.com"],
     "allow_headers": ["Content-Type", "Authorization"],
     "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }})
+
 
 db.init_app(app)
 jwt.init_app(app)
